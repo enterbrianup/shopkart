c246d4dc5a6f621bb8b773501e4f9ab03f5caf59
enterbrianup <anup.aky52@gmail.com>
2023-08-27 10:41:27 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+
+import Page from "./pages/Pages";
+
+const App = () => {
+ 
+  return (
+   <>
+   <Page />
+   </>
+    
+  );
+}
+export default App;
---
+++ src/pages/Pages.js
@@ -0,0 +1,197 @@
+import React from "react";
+import "./Pages.css";
+
+const Page = () => {
+  return (
+    <div className="desktop-1">
+      <div className="desktop-1-child" />
+      <img className="rectangle-icon" alt="" src="/rectangle@2x.png" />
+      {/* product image */}
+      <div className="rectangle-parent">
+        <div className="group-child" />
+        <img className="group-item" alt="" src="/star-3.svg" />
+        <img
+          className="jessica-radanavong-ichpbhfd0pw-icon"
+          alt=""
+          src="/jessicaradanavongichpbhfd0pwunsplash@2x.png"
+        />
+        <div className="group-inner" />
+        <img className="star-icon" alt="" src="/star-4.svg" />
+        <div className="oregon-jacket">Oregon jacket</div>
+        <div className="div">$124</div>
+        <div className="line-div" />
+      </div>
+      <img
+        className="zam-nungaray-acw3b7q6ys0-unspl-icon"
+        alt=""
+        src="/zamnungarayacw3b7q6ys0unsplash@2x.png"
+      />
+      <img
+        className="philipp-arlt-nmh9a0obon8-unspl-icon"
+        alt=""
+        src="/philipparltnmh9a0obon8unsplash@2x.png"
+      />
+      <img
+        className="philipp-arlt-8eyb-rvawty-unspl-icon"
+        alt=""
+        src="/philipparlt8eyb-rvawtyunsplash@2x.png"
+      />
+      {/* Shopkart heading wih wishlist and Bag */}
+      <div className="bag-0">BAG (0)</div>
+      <div className="wishlist-0">WISHLIST (0)</div>
+      <div className="shopkart">ShopKart</div>
+    {/* Main menu */}
+      <div className="home">HOME</div>
+      <div className="about">ABOUT</div>
+      <div className="dropdown">
+        <div className="dopdown">OUR PRODUCTS</div>
+        <div className="product-1">PRODUCT 1</div>
+        <div className="product-2">PRODUCT 2</div>
+        <div className="product-3">PRODUCT 3</div>
+        <div className="product-4">PRODUCT 4</div>
+        <div className="dropdown-child" />
+      </div>
+      <div className="contact-us">CONTACT US</div>
+      
+      {/* first page or banner Fresh Look 2002 */}
+      <div className="fresh">Fresh</div>
+      <div className="Look">Look</div>
+      <div className="year">2022</div>
+      {/* New products list  */}
+      
+      <div className="New_Products">New products</div>
+       <div className="apparel">Apparel</div>
+      <div className="accessories">Accessories</div>
+      <div className="best-sellers">Best sellers</div>
+      <div className="off">50% off</div>
+      <div>
+
+        {/* Product listing is here with product description */}
+      <div className="text-field">
+        <div className="florida-jacket-parent">
+          <div className="florida-jacket">FLORIDA JACKET</div>
+          <div className="suffered-alteration-in">
+            Suffered alteration in some form, bysuffalterattion in some forme,
+            byinjected humor, or randomised
+          </div>
+          <div className="florida-jacket">$100</div>
+        </div>
+      </div>
+      <div className="text-field1">
+        <div className="florida-jacket-parent">
+          <div className="florida-jacket">FLORIDA JACKET</div>
+          <div className="suffered-alteration-in">
+            Suffered alteration in some form, bysuffalterattion in some forme,
+            byinjected humor, or randomised
+          </div>
+          <div className="florida-jacket">$100</div>
+        </div>
+      </div>
+      <div className="text-field2">
+        <div className="florida-jacket-parent">
+          <div className="florida-jacket">FLORIDA JACKET</div>
+          <div className="suffered-alteration-in">
+            Suffered alteration in some form, bysuffalterattion in some forme,
+            byinjected humor, or randomised
+          </div>
+          <div className="florida-jacket">$100</div>
+        </div>
+      </div>
+      <div className="text-field3">
+        <div className="florida-jacket-parent">
+          <div className="florida-jacket">FLORIDA JACKET</div>
+          <div className="suffered-alteration-in">
+            Suffered alteration in some form, bysuffalterattion in some forme,
+            byinjected humor, or randomised
+          </div>
+          <div className="florida-jacket">$100</div>
+        </div>
+      </div>
+      </div>
+
+      <div className="desktop-1-item" />
+      <div className="desktop-1-inner" />
+      <img className="icon" alt="" src="/18591063@2x.png" />
+     
+    {/* Top Navbar */}
+      <div className="free-delivery">Free Delivery</div>
+      <div className="return-policy">Return Policy</div>
+      <div className="login">Login</div>
+      <div className="follow-us">Follow US</div>
+      <img className="layer-156-icon" alt="" src="/layer-156.svg" />
+      <img className="vector-icon" alt="" src="/vector.svg" />
+      <img className="vector-icon1" alt="" src="/vector1.svg" />
+      <img className="vector-icon2" alt="" src="/vector2.svg" />
+      <img className="group-icon" alt="" src="/group-6.svg" />
+      <div className="desktop-1-child1" />
+      <img className="desktop-1-child2" alt="" src="/group-16.svg" />
+      <img className="desktop-1-child3" alt="" src="/group-17.svg" />
+      {/* newsletter is here with input  */}
+      <div className="newslleter">Newsletter</div>
+      <div className="get-news-about-container">
+        <p className="blank-line">&nbsp;</p>
+        <p className="blank-line">
+          Get news about articles and updates in your inbox.
+        </p>
+      </div>
+      {/* Filling form */}
+      <input type="text" className="name" placeholder="NAME" />
+      <input type="email" className="email" placeholder="EMAIL" />
+      <textarea className="message" placeholder="MESSAGE"></textarea>
+
+      <img className="arrow-icon" alt="" src="/arrow-1.svg" />
+      <img className="desktop-1-child4" alt="" src="/arrow-2.svg" />
+      <div className="Get_in_container">
+        <p className="blank-line">{`GET `}</p>
+        <p className="blank-line">IN TOUCH</p>
+      </div>
+      {/* Button to submit */}
+      <div className="button">
+        <div className="button-child" />
+        <div className="send">SEND</div>
+      </div>
+      {/* giving property to INput section */}
+ 
+       <div className="desktop-1-child5" />
+      <div className="desktop-1-child6" />
+      <div className="desktop-1-child7" /> 
+      {/* Arrow ar product listing at righ bottom corner */}
+      
+      <div className="rectangle-group">
+        <div className="instance-child" />
+        <img className="vector-icon3" alt="" src="/vector3.svg" />
+      </div>
+      <div className="secondary-button">
+        <div className="instance-child" />
+        <img className="vector-icon3" alt="" src="/vector3.svg" />
+      </div>
+      <div className="secondary-button1">
+        <div className="instance-child" />
+        <img className="vector-icon3" alt="" src="/vector3.svg" />
+      </div>
+      <img
+        className="malicki-m-beser-pkmvkg7vnuo-un-icon"
+        alt=""
+        src="/malickimbeserpkmvkg7vnuounsplash@2x.png"
+      />
+    
+      <div className="frame-parent">
+        <div>
+          <div className="see-more-wrapper">
+            <div className="see-more">See more</div>
+          </div>
+          <img className="vector-icon7" alt="" src="/vector4.svg" />
+        </div>
+        <div className="instance-item" />
+      </div>
+     
+     
+      {/* footer page */}
+      <div className="copyright-2022-all">
+        Copyright 2022 All Right Reserved By SG
+      </div>
+    </div>
+  );
+};
+
+export default Page;
